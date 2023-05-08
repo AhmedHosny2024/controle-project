ab671636c668d483479c4727570a1af28fd02ef2
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2023-05-08 22:56:14 +0300

    add to 8 req

--- assignment.m
+++ assignment.m
@@ -60,6 +60,51 @@ end
 disp("and the poles are ");
 disp(X1_U_Poles);
 disp(isstable(X1_U))
+%req 4
+t=linspace(0,100,1000);
+% calculate the step response for X1 and X2
+[Y_X1,t_X1]=step(X1_U,t);
+[Y_X2,t_X2]=step(X2_U,t);
+%get steady state values 
+Steady_state_value_X1=Y_X1(end);
+Steady_state_value_X2=Y_X2(end);
+fprintf("steady state value for X1 %f \n",Steady_state_value_X1);
+fprintf("steady state value for X2 %f \n",Steady_state_value_X2);
+%end of req4
 
+G1=tf(-1,K2);
+G2=tf([M2,F2,K2+K3],1);
+G3=tf(1,K2);
+G4=tf([M1,F1,K1+K2],1);
+sysCommon=series(G2,G3);
+%system to X1
+sys1=feedback(series(G1,sysCommon),G4);
+%system to X2
+sys2=feedback(G1,series(sysCommon,G4));
+%req5
+sys3=feedback(sys2,1);
+X2_Xd=minreal(sys3);
+%end of req5
 
+%req6
+[Y_X2_XD,t_X2_XD]=step(2*X2_Xd,t);
+figure;
+plot(t_X2_XD,Y_X2_XD)
+%end of req6
+%req7
+stepinfo(X2_Xd);
+%end of req7
 
+%req 8
+t_values=[1,10,100,1000];
+for KP=t_values
+    sys3=feedback(sys2,KP);
+    X2_Xd=minreal(sys3);
+    [Y_X2_XD,t_X2_XD]=step(2*X2_Xd,t);
+    figure;
+    plot(t_X2_XD,Y_X2_XD)
+    stepinfo(X2_Xd)
+    Steady_State_Error=Y_X2_XD(end)-2;
+end
+
+%end of Req 8
